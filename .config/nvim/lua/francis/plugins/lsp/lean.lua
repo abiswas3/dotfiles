b3453b58175400f133f4b474bc2de11b2b6c81dc
The Cat <software@randomwalks.xyz>
2026-02-10 18:12:43 +0000

    Update fish

--- .config/nvim/lua/francis/plugins/lsp/lean.lua
+++ .config/nvim/lua/francis/plugins/lsp/lean.lua
@@ -9,14 +9,11 @@ return {
 
         -- a completion engine
         --    hrsh7th/nvim-cmp or Saghen/blink.cmp are popular choices
-
         -- 'nvim-telescope/telescope.nvim', -- for 2 Lean-specific pickers
         -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
         -- 'andrewradev/switch.vim',        -- for switch support
         -- 'tomtom/tcomment_vim',           -- for commenting
     },
-
-    ---@type lean.Config
     opts = { -- see below for full configuration options
         mappings = true,
     },
