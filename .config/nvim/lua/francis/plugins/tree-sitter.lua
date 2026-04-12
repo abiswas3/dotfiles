f8c3eb7ab6fafd05196e6f61f3fccfba7cd80473
Ari <software@randomwalks.xyz>
2026-04-12 21:48:17 +0100

    Fix tree-sitter API compatibility
    Removed broken new API check and simplified to use standard configs.setup
    approach that works across versions. Removed custom textobjects keybinding
    setup that tried to require non-existent modules.

--- .config/nvim/lua/francis/plugins/tree-sitter.lua
+++ .config/nvim/lua/francis/plugins/tree-sitter.lua
@@ -31,38 +31,31 @@ return {
                 'typst',
             }
 
-            -- Try new API first (nvim-treesitter main branch rewrite, Neovim 0.11+)
-            local has_new_api, ts = pcall(require, 'nvim-treesitter')
-            if has_new_api and ts.install then
-                ts.install(parsers)
-            else
-                -- Old API fallback
-                require('nvim-treesitter.configs').setup {
-                    ensure_installed = parsers,
-                    highlight = { enable = true },
-                    indent = { enable = true },
-                    textobjects = {
-                        select = {
-                            enable = true,
-                            lookahead = true,
-                            keymaps = {
-                                ['af'] = '@function.outer',
-                                ['if'] = '@function.inner',
-                                ['ac'] = '@class.outer',
-                                ['ic'] = '@class.inner',
-                            },
-                        },
-                        move = {
-                            enable = true,
-                            set_jumps = true,
-                            goto_next_start = { [']f'] = '@function.outer' },
-                            goto_next_end = { [']F'] = '@function.outer' },
-                            goto_previous_start = { ['[f'] = '@function.outer' },
-                            goto_previous_end = { ['[F'] = '@function.outer' },
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = parsers,
+                highlight = { enable = true },
+                indent = { enable = true },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ['af'] = '@function.outer',
+                            ['if'] = '@function.inner',
+                            ['ac'] = '@class.outer',
+                            ['ic'] = '@class.inner',
                         },
                     },
-                }
-            end
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+                        goto_next_start = { [']f'] = '@function.outer' },
+                        goto_next_end = { [']F'] = '@function.outer' },
+                        goto_previous_start = { ['[f'] = '@function.outer' },
+                        goto_previous_end = { ['[F'] = '@function.outer' },
+                    },
+                },
+            }
 
             -- Use bash parser for zsh files
             vim.treesitter.language.register('bash', 'zsh')
@@ -116,40 +109,5 @@ return {
         'nvim-treesitter/nvim-treesitter-textobjects',
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
         event = { 'BufReadPre', 'BufNewFile' },
-        config = function()
-            -- New API has standalone modules; old API configures via nvim-treesitter.configs (handled above)
-            local ok, select = pcall(require, 'nvim-treesitter-textobjects.select')
-            if not ok then
-                return
-            end
-
-            local move = require 'nvim-treesitter-textobjects.move'
-
-            vim.keymap.set({ 'x', 'o' }, 'af', function()
-                select.select_textobject('@function.outer', 'textobjects')
-            end, { desc = 'Select outer function' })
-            vim.keymap.set({ 'x', 'o' }, 'if', function()
-                select.select_textobject('@function.inner', 'textobjects')
-            end, { desc = 'Select inner function' })
-            vim.keymap.set({ 'x', 'o' }, 'ac', function()
-                select.select_textobject('@class.outer', 'textobjects')
-            end, { desc = 'Select outer class' })
-            vim.keymap.set({ 'x', 'o' }, 'ic', function()
-                select.select_textobject('@class.inner', 'textobjects')
-            end, { desc = 'Select inner class' })
-
-            vim.keymap.set({ 'n', 'x', 'o' }, ']f', function()
-                move.goto_next_start('@function.outer', 'textobjects')
-            end, { desc = 'Next function start' })
-            vim.keymap.set({ 'n', 'x', 'o' }, ']F', function()
-                move.goto_next_end('@function.outer', 'textobjects')
-            end, { desc = 'Next function end' })
-            vim.keymap.set({ 'n', 'x', 'o' }, '[f', function()
-                move.goto_previous_start('@function.outer', 'textobjects')
-            end, { desc = 'Prev function start' })
-            vim.keymap.set({ 'n', 'x', 'o' }, '[F', function()
-                move.goto_previous_end('@function.outer', 'textobjects')
-            end, { desc = 'Prev function end' })
-        end,
     },
 }
